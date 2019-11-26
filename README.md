e4bcdb6d169810bfcb753baa004b13fa3d0b1634
mikkelertbjerg <mikkelertbjerg@gmail.com>
2019-11-26 13:50:08 +0100

    Opened RestClient, not sure what changed.

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # gRPC vs. REST
 
 ## Abstract
+In a world with an ever growing amount of Microservices, gRPC claims to be faster and more stable than REST. Microservices can be heavily dependant on eachother, which means speed and stability is key. When gRPC claims faster than REST, why isn't it the de facto standard? In this blog we will put gRPC and REST head to head, to see which is actually faster.
 
 ## Introduction
 
+### Problem statement
+*Is gRPC faster than REST*
+
 ## Hypothesis
+*We will assume that gRPC is able to send and recieve requests faster than a tradiational REST.*
 
 ## REST
 "Følgende blog har peget på fordele/ulemper ved x, vi vil undersøge følgende n"
---
+++ RestForBlog2/RestClient/Program.cs
@@ -0,0 +1,30 @@
+﻿using RestForBlog2.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace RestClient
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string URL = "https://sub.domain.com/objects.json";
+            string urlParameters = "?api_key=123";
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(URL);
+
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = client.GetAsync(urlParameters).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                var dataObjects = response.Content. ReadAsAsync<IEnumerable<ObjectModel>>().Result;
+            }
+        }
+    }
+}
